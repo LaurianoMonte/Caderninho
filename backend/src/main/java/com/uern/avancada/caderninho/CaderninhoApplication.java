a8a643dfa84e2740779df3307fc0b87326414350
LaurianoMonte <laurianomonte@gmail.com>
2022-07-11 13:06:02 -0300

    Backend criado

---
+++ backend/src/main/java/com/uern/avancada/caderninho/CaderninhoApplication.java
@@ -0,0 +1,13 @@
+package com.uern.avancada.caderninho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaderninhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaderninhoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/uern/avancada/caderninho/CaderninhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uern.avancada.caderninho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaderninhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
